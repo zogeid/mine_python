576999b42681772a33096560feccd19a398efe44
dalares <zogeid@gmail.com>
2019-10-07 13:07:18 +0200

    Exe

--- StockConn.py
+++ StockConn.py
@@ -1,12 +1,15 @@
 import requests
-from StockDay import StockDay
 from StockMetaData import *
 from StockDayData import *
+
+
+
 
 
 class StockConn:
 
     def __init__(self, base_url=None, function=None, datatype=None, api_key=None, symbol=None):
+
         self.base_url = base_url
         self.function = function
         self.datatype = datatype
@@ -18,13 +21,17 @@ class StockConn:
             self.url = self.compose_url()
         self.stock_day_data = []
         self.metadata = None
+        self.resistances = []
+        self.current_resistance = None
+        self.supports = []
+        self.current_support = None
 
     def compose_url(self):
         url = self.base_url + '/query?function=' + self.function + '&symbol=' + self.symbol + '&datatype=' + self.datatype + '&apikey=' + self.api_key
         return url
 
     def connect(self):
-        self.stock_request = requests.get(self.url)
+        self.stock_request = requests.get(self.url, verify=False)
         if self.stock_request.status_code == 200:
             print("Connection done!")
             # print(self.stock_request.json())
@@ -61,18 +68,47 @@ class StockConn:
         else:
             return False
 
+    def set_resistance(self, day_data):
+        self.resistances.append(day_data)
+        self.current_resistance = day_data
+
+    def set_support(self, day_data):
+        self.supports.append(day_data)
+        self.set_support(day_data)
+    def is_resistance(self, day_data):
+        # Si no hay ninguna resistencia, es el primer valor, lo hacemos resistencia
+        if len(self.resistances) == 0:
+            self.set_resistance(day_data)
+            return True
+        else:
+            # Es mayor a la resistencia actual, tenemos nueva resistencia, la actualizamos y la añadimos
+            if day_data.high > self.current_resistance.high:
+                self.set_resistance(day_data)
+                return True
+        return False
+
+    def is_support(self, day_data):
+        # Si no hay ningun soporte, es el primer valor, lo hacemos soporte
+        if len(self.supports) == 0:
+            self.set_support(day_data)
+            return True
+        else:
+            # TODO no tengo clara la definicion
+            if day_data.high > self.current_resistance.high:
+                self.set_resistance(day_data)
+                return True
+        return False
+
     def get_supports_and_resistances(self):
+        turn_resistance = True
         for i, day_data in enumerate(self.historic_data):
             sdd = StockDayData(day_data)
-            if i < len(self.historic_data)-1 \
-                    and day_data.close < self.historic_data[i-1].close \
-                    and day_data.close < self.historic_data[i+1].close:
+            if turn_resistance and self.is_resistance(day_data):
+                    sdd.is_resistance = True
+                    turn_resistance = False
+            elif not turn_resistance and self.is_support(day_data):
                 sdd.is_support = True
-
-            if i < len(self.historic_data)-1 \
-                    and day_data.close > self.historic_data[i-1].close \
-                    and day_data.close > self.historic_data[i+1].close:
-                sdd.is_resistance = True
+                turn_resistance = True
 
             self.stock_day_data.append(sdd)
 
